a4f2abe8e4ea4966cdb4b6e560e6ab4ae6c23803
SINtyanneru <irjitgd@gmail.com>
2025-07-28 00:42:19 +0900

    私は何をしているのだろうか。

--- src/main/java/su/rumishistem/fediverse_gyotaku/Main.java
+++ src/main/java/su/rumishistem/fediverse_gyotaku/Main.java
@@ -3,7 +3,12 @@ package su.rumishistem.fediverse_gyotaku;
 import static su.rumishistem.rumi_java_lib.LOG_PRINT.Main.LOG;
 
 import java.io.File;
+import java.sql.Timestamp;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.function.Function;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -109,6 +114,113 @@ public class Main {
 			}
 		});
 
+		SH.SetRoute("/api/User", Method.GET, new EndpointFunction() {
+			@Override
+			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
+				if (r.GetEVENT().getURI_PARAM().get("ID") == null) {
+					return new HTTP_RESULT(400, "{\"STATUS\":false, \"ERR\":\"URI_PARAM_GA_TALINAI\"}".getBytes(), JsonMimetype);
+				}
+
+				ArrayNode sql_result = SQL.RUN("""
+					SELECT
+						P.*
+					FROM
+						`FG_USER_PROFILE` AS P
+					WHERE
+						P.USER = ?
+					ORDER BY
+						P.DATE DESC;
+				""", new Object[] {
+					r.GetEVENT().getURI_PARAM().get("ID")
+				});
+
+				List<Object> archive_list = new ArrayList<Object>();
+				for (int I = 0; I < sql_result.length(); I++) {
+					ArrayNode row = sql_result.get(I);
+					LinkedHashMap<String, Object> item = new LinkedHashMap<String, Object>();
+					item.put("ID", row.getData("ID").asString());
+					item.put("DATE", ((Timestamp)row.getData("DATE").asObject()).toInstant().atOffset(ZoneOffset.ofHours(9)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+					item.put("PREFERRED_UID", row.getData("PREFERRED_UID").asString());
+					item.put("NAME", row.getData("NAME").asString());
+					item.put("DESCRIPTION", row.getData("DESCRIPTION").asString());
+					item.put("ICON_ORIGINAL", row.getData("ICON_ORIGINAL").asString());
+					item.put("ICON_ARCHIVE", row.getData("ICON_ARCHIVE").asString());
+					item.put("HEADER_ORIGINAL", row.getData("HEADER_ORIGINAL").asString());
+					item.put("HEADER_ARCHIVE", row.getData("HEADER_ARCHIVE").asString());
+					archive_list.add(item);
+				}
+
+				LinkedHashMap<String, Object> return_body = new LinkedHashMap<String, Object>();
+				return_body.put("STATUS", true);
+				return_body.put("LIST", archive_list);
+				return new HTTP_RESULT(200, new ObjectMapper().writeValueAsString(return_body).getBytes(), JsonMimetype);
+			}
+		});
+
+		SH.SetRoute("/api/UserArchive", Method.GET, new EndpointFunction() {
+			@Override
+			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
+				if (r.GetEVENT().getURI_PARAM().get("ID") == null) {
+					return new HTTP_RESULT(400, "{\"STATUS\":false, \"ERR\":\"URI_PARAM_GA_TALINAI\"}".getBytes(), JsonMimetype);
+				}
+
+				ArrayNode sql_result = SQL.RUN("""
+					SELECT
+						P.*,
+						(SELECT `ID` FROM `FG_USER_PROFILE` WHERE `DATE` < P.DATE AND `USER` = P.USER ORDER BY `DATE` DESC LIMIT 1) AS BEFORE_ID,
+						(SELECT `ID` FROM `FG_USER_PROFILE` WHERE `DATE` > P.DATE AND `USER` = P.USER ORDER BY `DATE` ASC LIMIT 1) AS AFTER_ID,
+						I.HOST,
+						I_D.ID AS INSTANCE_ARCHIVE
+					FROM
+						`FG_USER_PROFILE` AS P
+					LEFT JOIN
+						`FG_INSTANCE_DATA` AS I_D
+						ON I_D.ID = P.INSTANCE_DATA
+					LEFT JOIN
+						`FG_INSTANCE` AS I
+						ON I.ID = I_D.INSTANCE
+					WHERE
+						P.ID = ?
+					ORDER BY
+						P.DATE DESC;
+				""", new Object[] {
+					r.GetEVENT().getURI_PARAM().get("ID")
+				});
+
+				ArrayNode row = sql_result.get(0);
+				if (row == null) return new HTTP_RESULT(404, "{\"STATUS\": false, \"ERR\": \"NTF\"}".getBytes(), JsonMimetype);
+
+				LinkedHashMap<String, Object> item = new LinkedHashMap<String, Object>();
+				item.put("ID", row.getData("ID").asString());
+				item.put("DATE", ((Timestamp)row.getData("DATE").asObject()).toInstant().atOffset(ZoneOffset.ofHours(9)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+				item.put("PREFERRED_UID", row.getData("PREFERRED_UID").asString());
+				item.put("NAME", row.getData("NAME").asString());
+				item.put("DESCRIPTION", row.getData("DESCRIPTION").asString());
+				item.put("ICON_ORIGINAL", row.getData("ICON_ORIGINAL").asString());
+				item.put("ICON_ARCHIVE", row.getData("ICON_ARCHIVE").asString());
+				item.put("HEADER_ORIGINAL", row.getData("HEADER_ORIGINAL").asString());
+				item.put("HEADER_ARCHIVE", row.getData("HEADER_ARCHIVE").asString());
+
+				LinkedHashMap<String, Object> return_body = new LinkedHashMap<String, Object>();
+				return_body.put("STATUS", true);
+				if (!row.getData("BEFORE_ID").isNull()) {
+					return_body.put("BEFORE", row.getData("BEFORE_ID").asString());
+				} else {
+					return_body.put("BEFORE", null);
+				}
+				if (!row.getData("AFTER_ID").isNull()) {
+					return_body.put("AFTER", row.getData("AFTER_ID").asString());
+				} else {
+					return_body.put("AFTER", null);
+				}
+
+				return_body.put("USER", item);
+				return_body.put("HOST", row.getData("HOST").asString());
+				return_body.put("INSTANCE", row.getData("INSTANCE_ARCHIVE").asString());
+				return new HTTP_RESULT(200, new ObjectMapper().writeValueAsString(return_body).getBytes(), JsonMimetype);
+			}
+		});
+
 		//エラー類
 		SH.SetRoute("/api/*", Method.ALL, new EndpointFunction() {
 			@Override
@@ -144,13 +256,19 @@ public class Main {
 			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
 				return new HTTP_RESULT(200, new RESOURCE_MANAGER(Main.class).getResourceData("/HTML/index.html"), HTMLMimetype);
 			}
-		});;
+		});
 		SH.SetRoute("/webui/user", Method.GET, new EndpointFunction() {
 			@Override
 			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
 				return new HTTP_RESULT(200, new RESOURCE_MANAGER(Main.class).getResourceData("/HTML/user.html"), HTMLMimetype);
 			}
-		});;
+		});
+		SH.SetRoute("/webui/user_archive", Method.GET, new EndpointFunction() {
+			@Override
+			public HTTP_RESULT Run(HTTP_REQUEST r) throws Exception {
+				return new HTTP_RESULT(200, new RESOURCE_MANAGER(Main.class).getResourceData("/HTML/user_archive.html"), HTMLMimetype);
+			}
+		});
 
 		SH.Start();
 	}
